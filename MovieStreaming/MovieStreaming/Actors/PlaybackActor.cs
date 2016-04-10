1cddec11726b95f2d371419393b9a83acb076d24
Luis Correa <luis.guillermo.correa@hotmail.com>
2016-04-10 12:17:33 -0500

    Initial commit

---
+++ MovieStreaming/MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using MovieStreaming.Messages;
+
+namespace MovieStreaming.Actors
+{
+    public class PlaybackActor : ReceiveActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating a PlaybackActor...");
+            ReceiveAsync<PlayMovieMessage>(message => HandlePlayMovieMessageAsync(message), message => message.UserId == 42);
+        }
+
+        private Task HandlePlayMovieMessageAsync(PlayMovieMessage message)
+        {
+            Console.WriteLine("Received: " + message.MovieTitle);
+            Console.WriteLine("Received User ID: " + message.UserId);
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ MovieStreaming/MovieStreaming/Program.cs
@@ -0,0 +1,28 @@
+﻿using Akka.Actor;
+using MovieStreaming.Actors;
+using MovieStreaming.Messages;
+using System;
+
+namespace MovieStreaming
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
+            Console.WriteLine("Actor system created");
+
+            Props playbackActorProps = Props.Create<PlaybackActor>();
+            IActorRef playbackActorRef = MovieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
+
+            playbackActorRef.Tell(new PlayMovieMessage("Akka the movie", 42));
+            playbackActorRef.Tell(new PlayMovieMessage("Akka the movie", 43));
+
+            Console.ReadLine();
+
+            MovieStreamingActorSystem.Terminate();
+        }
+    }
+}
