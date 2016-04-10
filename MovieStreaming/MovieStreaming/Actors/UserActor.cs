be55c6546f570ccd695d787c29950a979560c17e
Guillermo Correa <luis.guillermo.correa@hotmail.com>
2016-04-10 13:46:34 -0500

    Added UserActor

---
+++ MovieStreaming/MovieStreaming/Actors/UserActor.cs
@@ -0,0 +1,85 @@
+﻿using System.Threading.Tasks;
+using Akka.Actor;
+using System;
+using MovieStreaming.Messages;
+
+namespace MovieStreaming.Actors
+{
+    public class UserActor : ReceiveActor
+    {
+        private string _currentlyWatching;
+
+        public UserActor()
+        {
+            Console.WriteLine("Creating a UserActor");
+
+            ReceiveAsync<PlayMovieMessage>(message => HandlePlayMovieMessageAsync(message));
+            ReceiveAsync<StopMovieMessage>(message => HandleStopMovieMessageAsync(message));
+        }
+
+        private Task HandleStopMovieMessageAsync(StopMovieMessage message)
+        {
+            if (_currentlyWatching == null)
+            {
+                ColorConsole.WriteLineRed("Error: cannot stop movie if nothing is playing");
+            }
+            else
+            {
+                StopPlayingCurrentMovie();
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        private void StopPlayingCurrentMovie()
+        {
+            ColorConsole.WriteLineYellow(string.Format("User has stopped watching movie '{0}' ", _currentlyWatching));
+            _currentlyWatching = null;
+        }
+
+        private Task HandlePlayMovieMessageAsync(PlayMovieMessage message)
+        {
+            if (_currentlyWatching != null)
+            {
+                ColorConsole.WriteLineRed("Error: cannot start playing another movie before stopping existing one");
+            }
+            else
+            {
+                StartMoviePlaying(message.MovieTitle);
+            }
+
+            return Task.FromResult<object>(null);
+        }
+
+        private void StartMoviePlaying(string movieTitle)
+        {
+            _currentlyWatching = movieTitle;
+            ColorConsole.WriteLineYellow(string.Format("Currently watching '{0}'", _currentlyWatching));
+        }
+
+        protected override void PreStart()
+        {
+            ColorConsole.WriteLineGreen("UserActor PreStart");
+            base.PreStart();
+        }
+
+
+        protected override void PostStop()
+        {
+            ColorConsole.WriteLineGreen("UserActor PostStop");
+            base.PostStop();
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            ColorConsole.WriteLineGreen("UserActor PreRestart because: " + reason);
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            ColorConsole.WriteLineGreen("UserActor PostRestart because: " + reason);
+            base.PostRestart(reason);
+        }
+    }
+}
--- MovieStreaming/MovieStreaming/Program.cs
+++ MovieStreaming/MovieStreaming/Program.cs
@@ -15,15 +15,36 @@ namespace MovieStreaming
             MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem");
             Console.WriteLine("Actor system created");
 
+            Props userActorProps = Props.Create<UserActor>();
+            IActorRef userActorRef = MovieStreamingActorSystem.ActorOf(userActorProps, "UserActor");
+
+            Console.ReadKey();
+            Console.WriteLine("Sending a PlayMovieMessage (Codenan the destroyer)");
+            userActorRef.Tell(new PlayMovieMessage("Codenan the destroyer", 42));
+
+            Console.ReadKey();
+            Console.WriteLine("Sending a PlayMovieMessage (Boolean lies)");
+            userActorRef.Tell(new PlayMovieMessage("Boolean lies", 42));
+
+            Console.ReadKey();
+            Console.WriteLine("Sending a StopMovieMessage");
+            userActorRef.Tell(new StopMovieMessage());
+
+            Console.ReadKey();
+            Console.WriteLine("Sending another StopMovieMessage");
+            userActorRef.Tell(new StopMovieMessage());
+
+            /*
             Props playbackActorProps = Props.Create<PlaybackActor>();
             IActorRef playbackActorRef = MovieStreamingActorSystem.ActorOf(playbackActorProps, "PlaybackActor");
-
+            
             playbackActorRef.Tell(new PlayMovieMessage("Akka the movie", 42));
             playbackActorRef.Tell(new PlayMovieMessage("Fast N Furious", 43));
             playbackActorRef.Tell(new PlayMovieMessage("The Revenant", 44));
             playbackActorRef.Tell(new PlayMovieMessage("007", 45));
 
             playbackActorRef.Tell(PoisonPill.Instance);
+            */
 
             Console.ReadLine();
 
